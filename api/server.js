83144efae1200f8889410af01f57063049cc6dc9
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-04-19 10:11:10 +0700

    change port

--- api/server.js
+++ api/server.js
@@ -1,22 +1,23 @@
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
-
-// allow write operations
 const fs = require("fs");
 const path = require("path");
-// const filePath = path.join(__dirname + "db.json")
-const data = fs.readFileSync(__dirname + "db.json", "utf-8");
-const db = JSON.parse(data);
-const router = jsonServer.router(db);
 
+// Sửa đường dẫn của file db.json
+const filePath = path.join(__dirname, "db.json");
+
+// Đọc dữ liệu từ file db.json
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+
+const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
-const port = 3001; // you can use any port number here; i chose to use 3001
+const port = 3001;
 
 server.use(middlewares);
-// Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1'
-}))
+}));
 server.use(router);
 
 server.listen(port, () => {
