1b9fb31b4a8326ed876a60391d51fff0e1c496f1
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-04-19 10:09:01 +0700

    change port

--- api/server.js
+++ api/server.js
@@ -5,12 +5,12 @@ const server = jsonServer.create();
 const fs = require("fs");
 const path = require("path");
 // const filePath = path.join(__dirname + "db.json")
-const data = fs.readFileSync(__dirname + "/db.json", "utf-8");
+const data = fs.readFileSync(__dirname + "db.json", "utf-8");
 const db = JSON.parse(data);
 const router = jsonServer.router(db);
 
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
+const port = 3001; // you can use any port number here; i chose to use 3001
 
 server.use(middlewares);
 // Add this before server.use(router)
