e9592b72ea400654373414dd1fb863a99efe704c
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-04-19 10:25:28 +0700

    change port

--- api/server.js
+++ api/server.js
@@ -1,20 +1,47 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const filePath = path.join(__dirname, '../db.json');
-const data = fs.readFileSync(filePath, "utf-8");
-const router = jsonServer.router(data)
-const middlewares = jsonServer.defaults()
+// const jsonServer = require('json-server')
+// const server = jsonServer.create()
+// const filePath = path.join(__dirname, '../db.json');
+// const data = fs.readFileSync(__dirname, '../db.json', "utf-8");
+// const router = jsonServer.router(data)
+// const middlewares = jsonServer.defaults()
 
-server.use(middlewares)
+// server.use(middlewares)
+// // Add this before server.use(router)
+// server.use(jsonServer.rewriter({
+//     '/api/*': '/$1',
+//     '/product/:resource/:id/show': '/:resource/:id'
+// }))
+// server.use(router)
+// server.listen(3000, () => {
+//     console.log('JSON Server is running')
+// })
+
+// // Export the Server API
+// module.exports = server
+
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+
+// allow write operations
+const fs = require("fs");
+const path = require("path");
+// const filePath = path.join(__dirname + "db.json")
+const data = fs.readFileSync(__dirname + "../db.json", "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db);
+
+const middlewares = jsonServer.defaults();
+// const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
+
+server.use(middlewares);
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/product/:resource/:id/show': '/:resource/:id'
+    '/api/*': '/$1'
 }))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+server.use(router);
 
-// Export the Server API
-module.exports = server
+server.listen(3001, () => {
+    // console.log('JSON Server is running on port: ', 3);
+});
+
+module.exports = server;
